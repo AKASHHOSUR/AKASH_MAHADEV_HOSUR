bc867b71a1261de584f59d21dcaab7e12d3f0c6e
AKASHHOSUR <128850975+AKASHHOSUR@users.noreply.github.com>
2025-09-17 18:06:23 +0530

    Create Program-3.java
    Solution for Modified Odd Numbers

---
+++ Program-3.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class ModifiedOddNumbers {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter the range: ");
+    int range = sc.nextInt();
+    int limit = (a % 2 == 0) ? a - 1 : a;
+
+    for (int i = 1; i <= limit * 2; i += 2) {
+      if (i < limit * 2) {
+        System.out.print(i + ", ");
+      } else {
+        System.out.print(i);
+      }
+    }
+    sc.close();
+  }
+}
