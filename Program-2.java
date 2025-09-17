e96407dca192307034080160f88ffa1d3eba6644
AKASHHOSUR <128850975+AKASHHOSUR@users.noreply.github.com>
2025-09-17 17:57:48 +0530

    Create Program-2.java
    Solution for Printing Odd Numbers

---
+++ Program-2.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class OddNumbers {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter the range: ");
+    int range = sc.nextInt();
+
+    for (int i = 1; i <= range * 2; i += 2) {
+      if (i < range * 2) {
+        System.out.print(i + ", ");
+      } else {
+        System.out.print(i);
+      }
+    }
+    sc.close();
+  }
+}
